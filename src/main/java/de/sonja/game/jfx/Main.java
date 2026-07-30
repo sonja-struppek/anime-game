27303c53dfd8590c16c9d2ee2ea02a8c0a1315d2
SONJA STRUPPEK <sonja.struppek@web.de>
2026-07-30 15:48:29 +0200

    Add Player class and basic game loop

--- src/main/java/de/sonja/game/jfx/Main.java
+++ src/main/java/de/sonja/game/jfx/Main.java
@@ -1,16 +1,65 @@
 package de.sonja.game.jfx;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import de.sonja.game.jfx.model.Player;
 
 public class Main extends Application {
 
+    private Player player;
+
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 800, 600);
+
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case A:
+                case LEFT:
+                    player.moveLeft();
+                    break;
+
+                case D:
+                case RIGHT:
+                    player.moveRight();
+                    break;
+
+                case SPACE:
+                case W:
+                case UP:
+                    player.jump();
+                    break;
+            }
+        });
+
+        scene.setOnKeyReleased(event -> {
+            switch (event.getCode()) {
+                case A:
+                case LEFT:
+                case D:
+                case RIGHT:
+                    player.stopMoving();
+                    break;
+            }
+        });
+
+        // Player erstellen
+        player = new Player(100, 100);
+        root.getChildren().add(player);
+
+        // Game Loop
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                player.update();
+            }
+        };
+        gameLoop.start();
 
         stage.setTitle("Anime Jump and Run");
         stage.setScene(scene);
---
+++ src/main/java/de/sonja/game/jfx/model/Player.java
@@ -0,0 +1,58 @@
+package de.sonja.game.jfx.model;
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
+public class Player extends Rectangle {
+
+    // Position wird von Rectangle geerbt
+    private double velocityX = 0;
+    private double velocityY = 0;
+
+    private final double moveSpeed = 4;
+    private final double gravity = 0.4;
+    private final double jumpStrength = -10;
+
+    private boolean onGround = false;
+
+    public Player(double x, double y) {
+        super(40, 60, Color.BLUE); // Breite, Höhe, Farbe
+        setTranslateX(x);
+        setTranslateY(y);
+    }
+
+    public void update() {
+        // Schwerkraft anwenden
+        velocityY += gravity;
+
+        // Bewegung anwenden
+        setTranslateX(getTranslateX() + velocityX);
+        setTranslateY(getTranslateY() + velocityY);
+
+        // Boden-Kollision (temporär)
+        if (getTranslateY() >= 500) { // Boden bei y=500
+            setTranslateY(500);
+            velocityY = 0;
+            onGround = true;
+        }
+    }
+
+    public void moveLeft() {
+        velocityX = -moveSpeed;
+    }
+
+    public void moveRight() {
+        velocityX = moveSpeed;
+    }
+
+    public void stopMoving() {
+        velocityX = 0;
+    }
+
+    public void jump() {
+        if (onGround) {
+            velocityY = jumpStrength;
+            onGround = false;
+        }
+    }
+}
